4f953404474c13f7441a5b2dfe4db6a6e906244e
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2024-02-19 00:04:16 -0800

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Prog_OOP_Midterm_Part1_MatthewVargas
+{
+    public abstract class Product
+    {
+        protected Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public abstract void DisplayProductInfo();
+    }
+
+
+
+}
